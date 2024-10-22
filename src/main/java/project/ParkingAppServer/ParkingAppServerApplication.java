7c69fa023e01c6ff35f3af0922d07b492f4a43d3
lilballsmike <107635479+lilballsmike@users.noreply.github.com>
2024-10-22 17:02:43 +0200

    Initial commit

---
+++ src/main/java/project/ParkingAppServer/ParkingAppServerApplication.java
@@ -0,0 +1,13 @@
+package project.ParkingAppServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ParkingAppServer/ParkingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ParkingAppServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
