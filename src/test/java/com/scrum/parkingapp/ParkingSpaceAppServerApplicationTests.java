ab4e8200aa7a73e4ad45a9e486f9854908f1d6de
Musa-lee02 <samuelesiciliano.22@gmail.com>
2024-12-04 12:58:56 +0100

    Authentication tests

--- src/test/java/com/scrum/parkingapp/ParkingSpaceAppServerApplicationTests.java
+++ src/test/java/com/scrum/parkingapp/ParkingSpaceAppServerApplicationTests.java
@@ -1,15 +1,19 @@
 package com.scrum.parkingapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ImportAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class ParkingSpaceAppServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
